5a389498afc3ac37ddd1675e06bfa1bba35a5820
Vrinda Mavadhiya <vrindasm2005@gmail.com>
2026-01-28 11:07:34 +0530

    package changed

--- SQL_Masterclass/app/build.gradle.kts
+++ SQL_Masterclass/app/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 
 android {
     namespace = "com.project_vn.sql_masterclass"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.project_vn.sql_masterclass"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
