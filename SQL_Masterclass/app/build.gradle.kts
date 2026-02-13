09a9667645c24bc43b58469ba2da960ab54fbc13
Neeldeepsinh Jadeja <neeldeepsinhjadeja24@gmail.com>
2026-02-13 12:33:36 +0530

    GRADLE versioncode changed

--- SQL_Masterclass/app/build.gradle.kts
+++ SQL_Masterclass/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.project_vn.sql_masterclass"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
